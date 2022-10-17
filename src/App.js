dc0ab31e0e7de890e022eca68be0ca77130e9f74
Jonathan Salin Lee <jsalinlee@gmail.com>
2022-10-17 15:55:44 -0700

    Revert "Rough UI for player initiative input. Added some fonts"

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import CombatPage from './routes/combat-page/combat-page.component';
 import Navigation from './routes/navigation/navigation.component';
 import InitiativeSetup from './routes/initiative-setup/initiative-setup.component';
 
-const App = () => {
+  const App = () => {
     return (
-        <Routes>
-            <Route path='/' element={<Navigation />}>
-                <Route index element={<InitiativeSetup />} />
-                <Route path='combatpage' element={<CombatPage />} />
-            </Route>
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Navigation />} >
+            <Route index element={<InitiativeSetup />} />
+            <Route path='combatpage' element={<CombatPage/>} />
+        </Route>
+      </Routes>
     );
-};
+  };
 
 export default App;
