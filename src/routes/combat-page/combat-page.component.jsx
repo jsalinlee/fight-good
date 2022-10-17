e350a28b70d68e2a3dbf1a1bbb9ba4fc15fc7b17
Dmalonzo <malonzo.derek@gmail.com>
2022-10-17 13:00:05 -0700

    perfunctory routing set up.

---
+++ src/routes/combat-page/combat-page.component.jsx
@@ -0,0 +1,12 @@
+import './combat-page.styles.scss';
+
+const CombatPage = () =>{
+    return(
+        <div className='combat-container'>
+            <h1>THIS IS THE COMBAT PAGE</h1>
+            <button>button</button>
+        </div>
+    );
+};
+
+export default CombatPage;
