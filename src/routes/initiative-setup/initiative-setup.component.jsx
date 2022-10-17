065ce9de4088ae0a7c5cc90dd6ca8cffa26cc591
Dmalonzo <malonzo.derek@gmail.com>
2022-10-17 15:26:24 -0700

    Made basic initiative list UI

--- src/routes/initiative-setup/initiative-setup.component.jsx
+++ src/routes/initiative-setup/initiative-setup.component.jsx
@@ -1,9 +1,15 @@
+import InitiativeList from "../../components/initiative-list.component/initiative-list.component";
 
 
 const InitiativeSetup = () =>{
 
     return (
-        <div>This is where initiative setup goes</div>
+        <div>
+            This is where initiative setup goes
+            <InitiativeList />
+            
+        </div>
+        
     )
 
 
