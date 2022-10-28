d17e70b1ec2b6624b2439d1c6ea34162be15a225
Dmalonzo <malonzo.derek@gmail.com>
2022-10-28 13:35:19 -0700

    Basic framework for Redux

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux'
+import { store, persistor } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={store}>
+        <PersistGate persistor={persistor}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
     </React.StrictMode>
 );
 
