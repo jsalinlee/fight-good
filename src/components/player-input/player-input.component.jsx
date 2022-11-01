3a149b6e4a35305b5d5479159d66c5d53bb2a74f
jsalinlee <jsalinlee@gmail.com>
2022-11-01 13:42:27 -0700

    add player to initiative list functionality

--- src/components/player-input/player-input.component.jsx
+++ src/components/player-input/player-input.component.jsx
@@ -1,15 +1,43 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { addItemToInitiative } from '../../store/initiative-list/initiative-list.action';
+import { selectInitiativeListItems } from '../../store/initiative-list/initiative-list.selector';
 
 import './player-input.styles.scss';
 
+const PLAYER_INITIAL_STATE = {
+    name: '',
+    initiative: 0,
+    isPlayer: true,
+};
+
 const PlayerInput = () => {
     const dispatch = useDispatch();
-    // const initiativeListItems = useSelector();
+    const initiativeListItems = useSelector(selectInitiativeListItems);
+    const [player, setPlayer] = useState(PLAYER_INITIAL_STATE);
 
-    const addPlayerToInitiative = (event) => {
-        dispatch(addItemToInitiative());
+    const onPlayerNameChange = (event) => {
+        setPlayer({
+            ...player,
+            name: event.target.value,
+        });
+    };
+
+    const onPlayerInitiativeChange = (event) => {
+        setPlayer({
+            ...player,
+            initiative: event.target.value,
+        });
+    };
+
+    const addPlayerToInitiative = () => {
+        if (!player.name) {
+            alert("Player name can't be empty");
+            return;
+        }
+        dispatch(addItemToInitiative(initiativeListItems, player));
+        setPlayer(PLAYER_INITIAL_STATE);
     };
 
     return (
@@ -23,9 +51,11 @@ const PlayerInput = () => {
                 <div className='input-field-container'>
                     <label className='player-name-label'>Player Name</label>
                     <input
+                        onChange={onPlayerNameChange}
                         className='player-name-input'
                         type='text'
                         placeholder="Derrick Malon'Zo"
+                        value={player.name}
                     />
                 </div>
                 <div className='input-field-container'>
@@ -33,12 +63,16 @@ const PlayerInput = () => {
                         Initiative Roll
                     </label>
                     <input
+                        onChange={onPlayerInitiativeChange}
                         className='player-initiative-input'
                         type='number'
                         placeholder='0'
+                        value={player.initiative}
                     />
                 </div>
-                <button className='add-player'>&#10133;</button>
+                <button className='add-player' onClick={addPlayerToInitiative}>
+                    &#10133;
+                </button>
             </div>
         </div>
     );
