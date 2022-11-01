5fe13bfddcf5006d52ac86bbadf7622aae37da85
jsalinlee <jsalinlee@gmail.com>
2022-11-01 14:01:32 -0700

    reorder UI components to match figma design

--- src/routes/initiative-setup/initiative-setup.component.jsx
+++ src/routes/initiative-setup/initiative-setup.component.jsx
@@ -1,16 +1,19 @@
-import InitiativeList from "../../components/initiative-list/initiative-list.component";
-import PlayerInput from "../../components/player-input/player-input.component";
-import MonsterSearchBox from "../../components/monster-search-box/monster-search-box.component";
+import InitiativeList from '../../components/initiative-list/initiative-list.component';
+import PlayerInput from '../../components/player-input/player-input.component';
+import MonsterSearchBox from '../../components/monster-search-box/monster-search-box.component';
 
-const InitiativeSetup = () =>{
+import './initiative-setup.styles.scss';
+
+const InitiativeSetup = () => {
     return (
-        <div>
-            <PlayerInput />
-            This is where initiative setup goes
-            <InitiativeList />
+        <div className='initiative-setup-container'>
+            <div className='sidebar-container'>
+                <PlayerInput />
+                <InitiativeList />
+            </div>
             <MonsterSearchBox />
         </div>
-    )
-}
+    );
+};
 
 export default InitiativeSetup;
