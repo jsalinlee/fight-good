1ee31a03073467059a6c4de85bfaa8dfbd59d51a
Dmalonzo <malonzo.derek@gmail.com>
2022-11-07 15:00:56 -0800

    initiative tracker loop non-funcitonal at the moment

--- src/components/monster-stat-block/monster-stat-block.component.jsx
+++ src/components/monster-stat-block/monster-stat-block.component.jsx
@@ -1,16 +1,17 @@
+import './monster-stat-block.styles.scss';
+
 const MonsterStatBlock = ({ item }) => {
-    const { name, cr, size, type, alignment, ac, hp, init } = { item };
+    const { name, cr, size, type, alignment, ac, hp } = { item };
     return (
         <div>
             <h2>{name}</h2>
-            <div>
+            <div className='stat-block-wrapper'>
                 <span>Challenge Rating: {cr}</span>
                 <span>Size: {size}</span>
                 <span>Type: {type}</span>
                 <span>Alignment: {alignment}</span>
                 <span>Armor Class: {ac}</span>
-                <span> Health: {hp}</span>
-                <span> Initiative Modifier: {init}</span>
+                <span>Health: {hp}</span>
             </div>
         </div>
     );
