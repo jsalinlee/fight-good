44f83150cd79d6b5937c58e0c0c1bed668b937cf
jsalinlee <jsalinlee@gmail.com>
2022-11-14 13:07:00 -0800

    simple api service for getting monster stats

--- src/components/monster-stat-block/monster-stat-block.component.jsx
+++ src/components/monster-stat-block/monster-stat-block.component.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
+
+import { getMonsterStatsByName } from '../../utils/open5e-api-service/open5e-api-service';
+
 import './monster-stat-block.styles.scss';
 
 const MonsterStatBlock = ({ item }) => {
     const { name, cr, size, type, alignment, ac, hp } = { item };
+
+    useEffect(() => {
+        async function fetchData() {
+            const stats = await getMonsterStatsByName('Baphomet');
+            console.log(stats);
+        }
+        fetchData();
+    }, []);
+
     return (
         <div>
             <h2>{name}</h2>
