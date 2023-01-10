f65b33261ab99a9d203598add455c4bed2c6bec3
Dmalonzo <malonzo.derek@gmail.com>
2023-01-10 14:56:46 -0800

    monster search item bootstrap

--- src/components/monster-search-item/monster-search-item.component.jsx
+++ src/components/monster-search-item/monster-search-item.component.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-//import './monster-search-item.styles.scss';
+import './monster-search-item.styles.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectInitiativeListItems } from '../../store/initiative-list/initiative-list.selector';
 import { addItemToInitiative } from '../../store/initiative-list/initiative-list.action';
@@ -13,17 +13,19 @@ const MonsterSearchItem = ({ monster }) => {
         dispatch(addItemToInitiative(listItems, monster));
 
     return (
-        <div className='monster-search-item-container row'>
-            <span className='monster-name col-2'>{name}</span>
-            <span className='monster-cr col-2'>{cr}</span>
-            <span className='monster-size col-2'>{size}</span>
-            <span className='monster-type col-2'>{type}</span>
-            <button
-                className='monster-add-to-list col-1'
-                style={{ textDecoration: 'none' }}
-                onClick={addItemtoList}>
-                <b>Add</b>
-            </button>
+        <div className='monster-search-item-container container'>
+            <div className='row'>
+                <span className='monster-name col-4'>{name}</span>
+                <span className='monster-cr col-2'>{cr}</span>
+                <span className='monster-size col-2'>{size}</span>
+                <span className='monster-type col-2'>{type}</span>
+                <button
+                    className='monster-add-to-list col-1'
+                    style={{ textDecoration: 'none' }}
+                    onClick={addItemtoList}>
+                    <b>Add Group</b>
+                </button>
+            </div>
         </div>
     );
 };
